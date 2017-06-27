681afca82c5725bdd4e079ad45a23819e44428cb
Pranav Ashok <pranav.ashok@tum.de>
2017-06-28 04:41:29 +0530

    Added fuzzy search, improved looks

--- index.js
+++ index.js
@@ -24,16 +24,57 @@ app.get('/', function (req, res) {
 })
 
 app.get('/searching', function(req, res) {
-	val = req.query.search;
-	pool.query('SELECT konkani_word, english_word, subcategory FROM wordlist WHERE konkani_word LIKE \'' + val + '%\' OR english_word LIKE \'' + val + '%\'', '' , function(err, result) {
-	if(err) {
-		return console.error('error running query', err);
+	search_param = req.query.search;
+	// If typing in English, then
+	if (search_param.search(/^([x00-\xFF]+)/) != -1) {
+		primary_column = "english_word";
+		secondary_column = "konkani_word";
+	} else {
+		primary_column = "konkani_word";
+		secondary_column = "english_word";
 	}
+	
 	data = "";
-	result.rows.forEach(function(row) {
-		data += row.konkani_word + " " + row.english_word + "<br />";
-	}, this);
-	res.send(data);
+	pool.query('SELECT ' + primary_column + ' as primary, ' + secondary_column + ' as secondary FROM wordlist WHERE ' 
+				+ primary_column + ' LIKE \'' + search_param + '%\' ORDER BY ' + primary_column + ' LIMIT 8', "", function(err, result) {
+		if(err) {
+			return console.error('error running query', err);
+		}
+		data += "<table class=\"results-table\" id=\"dict-results-table\">";
+		if (result.rows.length == 0) {
+			data += "<thead><tr><td>No exact matches</td></tr></thead>";
+			data += "</thead>";
+		} else {
+			data += "<thead><tr><td>Dictionary-style matches</td></tr></thead>";
+			data += "<tbody>";
+			result.rows.forEach(function(row) {
+				data += "<tr><td>" + row.primary + "</td><td>" + row.secondary + "</td></tr>";
+			}, this);
+			data += "</tbody>";
+		}
+		data += "</table>";
+		pool.query('SELECT ' + primary_column + ' as primary, ' + secondary_column + ' as secondary FROM wordlist \
+			WHERE ' + primary_column + ' LIKE \'' + search_param + '%\' \
+			AND levenshtein(right(' + primary_column + ', -1), right(\'' + search_param + '\', -1)) BETWEEN 1 AND 2 \
+			ORDER BY ' + primary_column + ' LIMIT 8', "", function(err, result) {
+				if(err) {
+					return console.error('error running query', err);
+				}
+				data += "<table class=\"results-table\" id=\"suggested-results-table\">";
+				if (result.rows.length == 0) {
+					data += "<thead><tr><td>Try another query</td></tr></thead>";
+					data += "</thead>";
+				} else {
+					data += "<thead><tr><td>Suggested matches</td></tr></thead>";
+					data += "<tbody>";
+					result.rows.forEach(function(row) {
+						data += "<tr><td>" + row.primary + "</td><td>" + row.secondary + "</td></tr>";
+					}, this);
+					data += "</tbody>";
+				}
+				data += "</table>";
+				res.send(data);
+		});
 	});
 });
 
