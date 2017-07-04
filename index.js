ddb09ed9fcb83a2105f1d1e3ce0e69ee14742364
Pranav Ashok <pranav.ashok@tum.de>
2017-07-04 22:34:01 +0530

    Bind to PORT env variable

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ var express = require('express')
 var app = express()
 var path = require('path');
 
+app.set('port', (process.env.PORT || 3000));
+
 app.set('views', './views')
 app.set('view engine', 'pug')
 
