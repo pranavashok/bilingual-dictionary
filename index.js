2adb0f15d5674866afcfa04f92efdcd7735c59b0
Pranav Ashok <ashok@in.tum.de>
2019-04-29 20:35:20 +0200

    followed cors stuff from rollbar

--- index.js
+++ index.js
@@ -71,7 +71,12 @@ var rollbar = new Rollbar({
     }
 });
 
-// app.use(allowCrossDomain);
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 
 app.use(bodyParser.json()); // support json encoded bodies
 app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
